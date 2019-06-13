39279877010c72150d029c9ca61189e5413d568b
ermeck <ermeck@bk.ru>
2019-06-13 15:19:22 +0400

    lesson 2 created

---
+++ l-002/002.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>001 trepachev</title>
+</head>
+<body>
+
+    <p>
+        <?php
+            $a = 3;
+            echo $a;
+        ?>
+    </p>
+
+    <p><?php
+        $a = 10;
+        $b = 2;
+        echo $a + $b . '<br>';
+        echo $a - $b . '<br>';
+        echo $a * $b .'<br>';
+        echo $a / $b . '<br>';
+    ?></p>
+
+
+</body>
+</html>
