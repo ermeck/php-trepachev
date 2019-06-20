f322a761df6cdf5bdb38368c724af3e6568c22f4
ermeck <ermeck@bk.ru>
2019-06-20 21:36:07 +0400

    update 005

--- l-005/005.php
+++ l-005/005.php
@@ -124,6 +124,8 @@
             echo 'Sum = ' .$result;
         ?>
     </p>
+
+    <h2>Задачи</h2>
     <p>
         <h4>Task 10</h4>
         <?php 
@@ -136,31 +138,127 @@
         ?>
     </p>
     <p>
-        <h4>Task 11</h4><?php ?>
+        <h4>Task 11</h4>
+        <?php 
+            $arr = [-2, 5, -9, 15, -6, 4];
+            $sum = 0;
+            foreach ($arr as $value) {
+                if ($value < 0) {
+                    $sum += $value;
+                }
+            }
+            echo $sum;
+        ?>
     </p>
     <p>
-        <h4>Task 12</h4><?php ?>
+        <h4>Task 12</h4>
+        <?php 
+            $arr = [1, 2, 5, 9, 4, 13, 4, 10];
+            foreach ($arr as $value) {
+                $i++;
+                if ($value === 4) {
+                    echo "Theare is 4 in array";
+                    break;
+                }
+            }
+        ?>
     </p>
     <p>
-        <h4>Task 13</h4><?php ?>
+        <h4>Task 13</h4>
+        <?php 
+            $arr = [10, 20, 30, 50, 235, 3000];
+            foreach ($arr as $value) {
+                $str = (string)$value;
+                if ($str[0] == 1 || $str[0] == 2 || $str[0] == 5) {
+                    echo $value . '<br>';
+                }
+            }
+        ?>
     </p>
     <p>
-        <h4>Task 14</h4><?php ?>
+        <h4>Task 14</h4>
+        <?php 
+            $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            $str = '';
+            foreach ($arr as $value) {
+                $str .= -$value;
+            }
+            echo $str;
+        ?>
     </p>
     <p>
-        <h4>Task 15</h4><?php ?>
+        <h4>Task 15</h4>
+        <?php 
+            $week = ['пн', "вт", "ср", "чт", "пт", "сб", "вс"];
+            foreach ($week as $day) {
+                if ($day === "сб" || $day === "вс") 
+                    echo '<p><b>'.$day.'</b></p>';
+                else 
+                    echo '<p>'.$day.'<p>';
+            }
+        ?>
     </p>
     <p>
-        <h4>Task 16</h4><?php ?>
+        <h4>Task 16</h4>
+        <?php 
+            $week = ['пн', "вт", "ср", "чт", "пт", "сб", "вс"];
+            $day = 'чт';
+            foreach ($week as $value) {
+                if ($value === $day) 
+                    echo '<p><i>'.$value.'</i></p>';
+                else 
+                    echo '<p>'.$value.'<p>';
+            }
+        ?>
     </p>
     <p>
-        <h4>Task 17</h4><?php ?>
+        <h4>Task 17</h4>
+        <?php 
+            $arr = [];
+            for ($i = 1; $i <= 10; $i++) {
+                $arr[] = $i;
+            }
+            echo '<pre>';
+            print_r($arr);
+            echo '</pre>';
+        ?>
     </p>
     <p>
-        <h4>Task 18</h4><?php ?>
+        <h4>Task 18</h4>
+        <?php 
+            $arr = ['green'=>'зеленый', 'red'=>'красный', 'blue'=>'голубой'];
+            $en = []; 
+            $ru = [];
+            foreach ($arr as $key => $value) {
+                $en[] = $key;
+                $ru[] = $value;
+            }
+            echo '<pre>';
+            print_r($en);
+            print_r($ru);
+            echo '</pre>';
+        ?>
     </p>
     <p>
-        <h4>Task 19</h4><?php ?>
+        <h4>Task 19</h4>
+        <?php 
+            $num = 1000;
+            $i = 0;
+            while ($num > 50) {
+                $i++;
+                $num /= 2;
+            }
+            echo '$num = '. $num . ', $i = ' . $i . '.<br>';
+
+            $num = 1000;
+            for ($i = 1; $i < 100; $i++) {
+                $num /= 2;
+                if ($num < 50) {
+                    echo '$num = '. $num . ', $i = ' . $i . '.';
+                    break;
+                }
+            }
+        ?>
     </p>
 </body>
 </html>
