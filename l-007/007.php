9ee3831d9ac7f302f2572a72646aa30e846bb663
ermeck <ermeck@bk.ru>
2019-06-22 07:52:10 +0400

    l 07

--- l-007/007.php
+++ l-007/007.php
@@ -2,9 +2,196 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>007</title>
 </head>
 <body>
-    
+    <h2>Работа с регистром символов</h2>
+    <p>
+        <h4>Task 1</h4>
+        <?php
+            $str = 'php';
+            echo strtoupper($str);
+        ?>
+    </p>
+    <p>
+        <h4>Task 2</h4>
+        <?php
+            echo strtolower('PHP');
+        ?>
+    </p>
+    <p>
+        <h4>Task 3</h4>
+        <?php
+            echo usfirst('london');
+        ?>
+    </p>
+    <p>
+        <h4>Task 4</h4>
+        <?php
+            echo lcfirst('London');
+        ?>
+    </p>
+    <p>
+        <h4>Task 5</h4>
+        <?php
+            echo ucwords('london is the capital of great britain');
+        ?>
+    </p>
+
+    <h4>Работа с strlen</h4>
+    <p>
+        <h4>Task 6</h4>
+    </p>
+    <p>
+        <h4>Task 7</h4>
+    </p>
+    <p>
+        <h4>Task 8</h4>
+    </p>
+    <p>
+        <h4>Task 9</h4>
+    </p>
+    <p>
+        <h4>Task 10</h4>
+    </p>
+    <p>
+        <h4>Task 11</h4>
+    </p>
+    <p>
+        <h4>Task 12</h4>
+    </p>
+    <p>
+        <h4>Task 13</h4>
+    </p>
+    <p>
+        <h4>Task 14</h4>
+    </p>
+    <p>
+        <h4>Task 15</h4>
+    </p>
+    <p>
+        <h4>Task 16</h4>
+    </p>
+    <p>
+        <h4>Task 17</h4>
+    </p>
+    <p>
+        <h4>Task 18</h4>
+    </p>
+    <p>
+        <h4>Task 19</h4>
+    </p>
+    <p>
+        <h4>Task 20</h4>
+    </p>
+    <p>
+        <h4>Task 21</h4>
+    </p>
+    <p>
+        <h4>Task 22</h4>
+    </p>
+    <p>
+        <h4>Task 23</h4>
+    </p>
+    <p>
+        <h4>Task 24</h4>
+    </p>
+    <p>
+        <h4>Task 25</h4>
+    </p>
+    <p>
+        <h4>Task 26</h4>
+    </p>
+    <p>
+        <h4>Task 27</h4>
+    </p>
+    <p>
+        <h4>Task 28</h4>
+    </p>
+    <p>
+        <h4>Task 29</h4>
+    </p>
+    <p>
+        <h4>Task 30</h4>
+    </p>
+    <p>
+        <h4>Task 31</h4>
+    </p>
+    <p>
+        <h4>Task 32</h4>
+    </p>
+    <p>
+        <h4>Task 33</h4>
+    </p>
+    <p>
+        <h4>Task 34</h4>
+    </p>
+    <p>
+        <h4>Task 35</h4>
+    </p>
+    <p>
+        <h4>Task 36</h4>
+    </p>
+    <p>
+        <h4>Task 37</h4>
+    </p>
+    <p>
+        <h4>Task 38</h4>
+    </p>
+    <p>
+        <h4>Task 39</h4>
+    </p>
+    <p>
+        <h4>Task 40</h4>
+    </p>
+    <p>
+        <h4>Task 41</h4>
+    </p>
+    <p>
+        <h4>Task 42</h4>
+    </p>
+    <p>
+        <h4>Task 43</h4>
+    </p>
+    <p>
+        <h4>Task 44</h4>
+    </p>
+    <p>
+        <h4>Task 45</h4>
+    </p>
+    <p>
+        <h4>Task 46</h4>
+    </p>
+    <p>
+        <h4>Task 47</h4>
+    </p>
+    <p>
+        <h4>Task 48</h4>
+    </p>
+    <p>
+        <h4>Task 49</h4>
+    </p>
+    <p>
+        <h4>Task 50</h4>
+    </p>
+    <p>
+        <h4>Task 51</h4>
+    </p>
+    <p>
+        <h4>Task 52</h4>
+    </p>
+    <p>
+        <h4>Task 53</h4>
+    </p>
+    <p>
+        <h4>Task 54</h4>
+    </p>
+    <p>
+        <h4>Task 55</h4>
+    </p>
+    <p>
+        <h4>Task 56</h4>
+    </p>
+
 </body>
 </html> 
