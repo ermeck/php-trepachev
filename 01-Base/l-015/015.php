8052d9f08896cc215d315755d0fd1ae6f84fbb02
ermeck <ermeck@bk.ru>
2019-07-03 17:11:57 +0400

    l 015

---
+++ 01-Base/l-015/015.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>015. Вложенные циклы</title>
+</head>
+<body>
+    <p>
+        <h2>Task 1</h2>
+        <?php 
+            
+        ?>
+    </p>
+    <p>
+        <h2>Task 2</h2>
+        <?php 
+
+        ?>
+    </p>
+    <p>
+        <h2>Task 3</h2>
+        <?php 
+
+        ?>
+    </p>
+    <p>
+        <h2>Task 4</h2>
+        <?php 
+
+        ?>
+    </p>
+    <p>
+        <h2>Task 5</h2>
+        <?php 
+
+        ?>
+    </p>
+    <p>
+        <h2>Task 6</h2>
+        <?php 
+
+        ?>
+    </p>
+</body>
+</html>
